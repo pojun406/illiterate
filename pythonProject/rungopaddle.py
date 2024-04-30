1c66944508fdbca8cdd091412fc8db4f8f53ac4d
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-04-30 15:45:12 +0900

    get OCR
    do mistysclouds
    
    Co-Authored-By: mistysclouds <131018815+mistysclouds@users.noreply.github.com>

--- pythonProject/rungopaddle.py
+++ pythonProject/rungopaddle.py
@@ -1,8 +1,3 @@
 from letsgopaddle import MyPaddleOCR
 
 ocr = MyPaddleOCR()
-
-img_path = 'C:/Users/404ST011/PycharmProjects/pythonProject/photo/456.jpg'
-result = ocr.run_ocr(img_path, debug=True)
-
-print(result)
