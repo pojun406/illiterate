9e8eb9f92058d10029967f3d2cc173caf3daacac
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-05-09 05:59:57 +0900

    import tailwindcss
    tailwind css 추가

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"], // Using 'sans' as an example
+                courgette: ["Courgette", "cursive"],
+            },
+        },
+    },
+    plugins: [
+        require("tailwindcss"),
+        require("postcss-preset-env")({
+            stage: 0,
+        }),
+    ],
+    purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+};
