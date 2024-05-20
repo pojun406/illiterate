a800c9002bd0a0bd012c400e28b828d395d42c68
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-20 15:55:31 +0900

    Revert "Troubleshooting signup button function errors"
    This reverts commit 8f0d3bb065e28c3e499bb0d8853128ad68f97dfb.

--- frontend/src/Components/SignupForm/SignupForm.tsx
+++ frontend/src/Components/SignupForm/SignupForm.tsx
@@ -2,10 +2,8 @@ import React, { useState } from "react";
 import UserIdInput from "./UserIdInput";
 import PasswordInput from "./PasswordInput";
 import PhoneNumberInput from "./PhoneNumberInput";
-import UserNameInput from "./UserNameInput";
 
 const SignupForm: React.FC = () => {
-    const [userid, setUserid] = useState<string>("");
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [phoneNumber, setPhoneNumber] = useState<string>("");
@@ -18,17 +16,14 @@ const SignupForm: React.FC = () => {
         setPhoneNumber(phoneNumber);
     };
 
-    const handleVerificationCodeSend = (isCodeSent:boolean): void => {
-        setIsCodeSent(isCodeSent);
+    const handleVerificationCodeSend = (): void => {
+        setIsCodeSent(true);
     };
 
-    const handleVerificationCodeChange = (verificationCode: string): void => {
-        setVerificationCode(verificationCode);
+    const handleVerificationCodeChange = (code: string): void => {
+        setVerificationCode(code);
     };
 
-    const handleUseridChange = (userid: string): void => {
-        setUserid(userid);
-    };
     const handleUsernameChange = (username: string): void => {
         setUsername(username);
     };
@@ -43,17 +38,12 @@ const SignupForm: React.FC = () => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault();
-        console.log("Userid:", userid);
         console.log("Username:", username);
         console.log("Password:", password);
         console.log("Phone Number:", phoneNumber);
         console.log("Verification Code:", verificationCode);
         console.log("Selected Carrier:", selectedCarrier);
-        console.log("isUsernameValid:", isUsernameValid);
     };
-    const handleTest = () =>{
-        console.log("Userid:", userid);
-    }
 
     return (
         <div>
@@ -61,22 +51,21 @@ const SignupForm: React.FC = () => {
                 <div className="px-4 py-6">
                     <div className="text-center font-bold text-blue-300 text-xl mb-4">회원가입</div>
                     <form onSubmit={handleSubmit}>
-                        <UserNameInput
-                            onUsernameChange={handleUsernameChange}/>
                         <UserIdInput
-                            onUseridChange={handleUseridChange}
-                            UseridValid={handleUsernameValidation}
+                            onUsernameChange={handleUsernameChange}
+                            UsernameValid={handleUsernameValidation}
                         />
                         <PasswordInput setPassword={setPassword} />
                         <PhoneNumberInput
                             onPhoneNumberChange={handlePhoneNumberChange}
                             onVerificationCodeSend={handleVerificationCodeSend}
+                            isCodeSent={isCodeSent}
                             onVerificationCodeChange={handleVerificationCodeChange}
-                            onSelectCarrier={handleSelectCarrier}
+                            onSelectCarrier={handleSelectCarrier} // Pass handleSelectCarrier function
                         />
                         <button
+                            type="submit"
                             className="w-full mt-4 px-4 py-2 text-white bg-blue-700 rounded-md hover:bg-blue-800 focus:outline-none"
-                            onClick={handleTest}
                         >
                             회원가입
                         </button>
