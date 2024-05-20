ed6dba356f77d34e93dbac0da999356925f7805a
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-20 15:55:27 +0900

    Revert "Merge branch 'front' of https://github.com/pojun406/illiterate into front"
    This reverts commit 5f9cbd050b001fabf238d7d7300aaf5834a85bf0, reversing
    changes made to 8f0d3bb065e28c3e499bb0d8853128ad68f97dfb.

--- src/main/java/com/illiterate/illiterate/testController/TestController.java
+++ src/main/java/com/illiterate/illiterate/testController/TestController.java
@@ -1,6 +1,5 @@
 package com.illiterate.illiterate.testController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,10 +7,5 @@ public class TestController {
     @GetMapping("/api/test")
     public String hello() {
         return "테스트입니다.";
-    }
-
-    @PostMapping("/api/test/python")
-    public String test_py(){
-        ProcessBuilder processBuilder = new ProcessBuilder("python", "test.py", "arg1","agr2");
     }
 }
