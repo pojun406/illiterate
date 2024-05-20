7ab00b6420c839b128e75055aa08a57706a71f3e
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-20 15:56:01 +0900

    Reapply "Troubleshooting signup button function errors"
    This reverts commit 709803690a74b6e7dc8c654fc5262b6eaf5ab8e7.

--- frontend/src/Components/SignupForm/UserIdInput.tsx
+++ frontend/src/Components/SignupForm/UserIdInput.tsx
@@ -2,25 +2,23 @@ import React, { useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
 
 interface UserIdInputProps {
-    onUsernameChange: (username: string) => void;
-    UsernameValid: (isValid: boolean) => void;
+    onUseridChange: (userid: string) => void;
+    UseridValid: (isValid: boolean) => void;
 }
 
-const UserIdInput: React.FC<UserIdInputProps> = ({ onUsernameChange, UsernameValid }) => {
-    const [username, setUsername] = useState("");
-    const [isUsernameValid, setIsUsernameValid] = useState(true);
+const UserIdInput: React.FC<UserIdInputProps> = ({ onUseridChange, UseridValid }) => {
+    const [userId, setUserId] = useState("");
+    const [isUseridValid, setIsUseridValid] = useState(true);
 
-    const handleCheckUsername = () => {
-        // Assuming you have a function to check username validity
-        // Here, for demonstration, let's assume it's always valid
-        setIsUsernameValid(true);
-        UsernameValid(true);
+    const handleCheckUserid = () => {
+        setIsUseridValid(true);
+        UseridValid(true);
     };
 
-    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
-        onUsernameChange(e.target.value);
-        setIsUsernameValid(true);
+    const handleUseridChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserId(e.target.value);
+        onUseridChange(e.target.value);
+        setIsUseridValid(true);
     };
 
     return (
@@ -29,17 +27,18 @@ const UserIdInput: React.FC<UserIdInputProps> = ({ onUsernameChange, UsernameVal
             <input
                 type="text"
                 placeholder="아이디"
-                value={username}
-                onChange={handleUsernameChange}
+                value={userId}
+                onChange={handleUseridChange}
                 className="flex-1 px-4 py-2 ml-2 focus:outline-none rounded-l-md rounded-r-md"
             />
             <button
-                onClick={handleCheckUsername}
+                type="button"
+                onClick={handleCheckUserid}
                 className="px-4 py-2 ml-2 bg-gray-300 rounded-md hover:bg-gray-400 focus:outline-none"
             >
                 중복확인
             </button>
-            {!isUsernameValid && (
+            {!isUseridValid && (
                 <div className="text-red-500 ml-2">이미 사용 중인 아이디입니다.</div>
             )}
         </div>
