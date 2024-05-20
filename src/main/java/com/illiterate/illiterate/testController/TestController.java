7e1aa22a98cc09636663b48b85389c3f8fc4da69
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-20 14:41:17 +0900

    .

--- src/main/java/com/illiterate/illiterate/testController/TestController.java
+++ src/main/java/com/illiterate/illiterate/testController/TestController.java
@@ -1,5 +1,6 @@
 package com.illiterate.illiterate.testController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,5 +8,10 @@ public class TestController {
     @GetMapping("/api/test")
     public String hello() {
         return "테스트입니다.";
+    }
+
+    @PostMapping("/api/test/python")
+    public String test_py(){
+        ProcessBuilder processBuilder = new ProcessBuilder("python", "test.py", "arg1","agr2");
     }
 }
