da1bf18b56672cf4d9f4c64c1a38de0a74ab030a
asowjdan <asowjdan@naver.com>
2024-05-21 16:48:40 +0900

    test

--- src/main/java/com/illiterate/illiterate/Service/UserService.java
+++ src/main/java/com/illiterate/illiterate/Service/UserService.java
@@ -1,5 +1,6 @@
 package com.illiterate.illiterate.Service;
 
+import com.illiterate.illiterate.DTO.UserDto;
 import com.illiterate.illiterate.Entity.User;
 import com.illiterate.illiterate.JWT.JwtProvider;
 import com.illiterate.illiterate.Repository.UserRepository;
@@ -8,9 +9,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
+
+    private static final Logger LOGGER = Logger.getLogger(UserService.class.getName());
+
     @Autowired
     private UserRepository userRepository;
 
@@ -20,30 +25,41 @@ public class UserService {
     @Autowired
     private JwtProvider jwtProvider;
 
-    public User registerUser(User UserDTO) {
+    public User registerUser(UserDto userDto) {
         // 비밀번호를 암호화하여 저장
         User user = new User();
-        user.setUid(UserDTO.getUid());
-        user.setPassword(passwordEncoder.encode(UserDTO.getPassword()));
+        user.setUid(userDto.getUserid());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setPhonenum(userDto.getPhonenum());
         return userRepository.save(user);
     }
 
     public String generateToken(String username) {
-        return jwtProvider.generateToken(username);
+        String token = jwtProvider.generateToken(username);
+        LOGGER.info("Generated Token: " + token);
+        return token;
     }
 
-    public String authenticate(String uid, String password) {
-        Optional<User> userId = userRepository.findByUid(uid);
-        Optional<User> userPw = userRepository.findByPassword(password);
-        if (userId.isPresent() && userPw.isPresent()){
-            String userid = uid;
-            return generateToken(userid); // 인증 성공
-        } else{
-            return null; // 인증 실패
+    public String authenticate(String userid, String password) {
+        LOGGER.info("Authenticating user: " + userid);
+        Optional<User> optionalUser = userRepository.findByUid(userid);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                String token = generateToken(userid);
+                LOGGER.info("Authentication successful for user: " + userid);
+                return token;
+            } else {
+                LOGGER.warning("Password mismatch for user: " + userid);
+            }
+        } else {
+            LOGGER.warning("User not found: " + userid);
         }
+        return null; // 인증 실패
     }
 
     public void userProcess(User user) {
+        // TODO: 추가 로직 구현
     }
 }
-
