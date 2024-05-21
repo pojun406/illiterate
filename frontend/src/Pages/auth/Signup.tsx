9b0fed3215fe037aabb23b5ee8a1c67c5c6e55a7
asowjdan <asowjdan@naver.com>
2024-05-21 14:56:25 +0900

    Add login, membership page responsive

--- frontend/src/Pages/auth/Signup.tsx
+++ frontend/src/Pages/auth/Signup.tsx
@@ -1,16 +1,12 @@
-// Login.tsx
-
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../Components/Logo/Logo";
 import SignupForm from "../../Components/SignupForm/SignupForm";
 
 const Signup = () => {
-    const [activeTab, setActiveTab] = useState("basic");
-
     return (
         <div className="flex min-h-screen items-center justify-center px-4">
-            <div className="flow-root">
+            <div className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto">
                 <div className="flex items-center justify-center my-4">
                     <Link to="/">
                         <Logo />
