bab95b6d365dce3fb8495c18a632d96d95eec994
asowjdan <asowjdan@naver.com>
2024-05-22 19:35:10 +0900

    test

---
+++ src/main/java/com/illiterate/illiterate/Service/PasswordEncoderExample.java
@@ -0,0 +1,13 @@
+package com.illiterate.illiterate.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderExample {
+    public static void main(String[] args) {
+        String rawPassword = "examplePasswordHash";
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        System.out.println("Encoded Password: " + encodedPassword);
+    }
+}
