750c7a81752e0cfb0d07f492ed8ff37cbe4f3b20
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-22 19:03:55 +0900

    error fix
    httpServlet -> id = username
    not userid

--- src/main/java/com/illiterate/illiterate/Controller/UserController.java
+++ src/main/java/com/illiterate/illiterate/Controller/UserController.java
@@ -42,19 +42,5 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public void loginUser(@RequestParam String id, @RequestParam String password, HttpServletResponse response) throws ServletException, IOException {
-        try {
-            // 로그인 요청을 LoginFilter를 통해 처리
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(id, password));
-
-            // 인증에 성공한 경우 JWT 토큰 생성
-            String token = response.getHeader("Authorization").substring(7); // "Bearer " 이후의 토큰 값만 추출
-
-            // 토큰을 클라이언트에게 반환
-            response.getWriter().write(token);
-        } catch (AuthenticationException e) {
-            // 인증에 실패한 경우
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        }
-    }
+    public void loginUser() {}
 }
