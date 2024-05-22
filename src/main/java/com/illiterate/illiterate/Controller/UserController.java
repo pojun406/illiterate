9c5ffa72f36024123eee1ac5b8dd4e3ac5785d31
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-22 20:37:43 +0900

    Revert "."
    This reverts commit 18c1894ff56d2ce7b46e132389dd465376e82a40.

--- src/main/java/com/illiterate/illiterate/Controller/UserController.java
+++ src/main/java/com/illiterate/illiterate/Controller/UserController.java
@@ -3,9 +3,22 @@ package com.illiterate.illiterate.Controller;
 
 import com.illiterate.illiterate.DTO.JoinDto;
 import com.illiterate.illiterate.JWT.JWTUtil;
+import com.illiterate.illiterate.Service.CustomUserDetailsService;
 import com.illiterate.illiterate.Service.JoinService;
+import com.illiterate.illiterate.Service.LoginService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
