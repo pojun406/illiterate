412b8231efec551e7f90c4770add801b7ac3c9a6
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-22 15:04:15 +0900

    update controller
    update login

--- src/main/java/com/illiterate/illiterate/Controller/UserController.java
+++ src/main/java/com/illiterate/illiterate/Controller/UserController.java
@@ -2,16 +2,27 @@ package com.illiterate.illiterate.Controller;
 
 
 import com.illiterate.illiterate.DTO.JoinDto;
-import com.illiterate.illiterate.Entity.User;
+import com.illiterate.illiterate.JWT.JWTUtil;
 import com.illiterate.illiterate.Service.JoinService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JWTUtil jwtUtil;
 
     private final JoinService joinService;
 
@@ -24,13 +35,22 @@ public class UserController {
         joinService.joinProcess(joinDTO);
         return "ok";
     }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
-        String token = userService.authenticate(email, password);
-        if (token != null) {
-            return ResponseEntity.ok("로그인 성공: " + token);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 자격 증명");
+    public Map<String, String> login(@RequestBody Map<String, String> loginData) {
+        try {
+            String username = loginData.get("username");
+            String password = loginData.get("password");
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            String token = jwtUtil.createJwt(username, 3600000L); // 수명 1시간
+            Map<String, String> response = new HashMap<>();
+            response.put("token", token);
+            System.out.println(token + " <= 토큰값");
+            return response;
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("자격증명 오류");
         }
     }
 }
