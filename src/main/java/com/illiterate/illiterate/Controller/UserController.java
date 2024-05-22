a61621861f50b15cd9a3cee3537536c833cfde95
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-22 18:51:34 +0900

    .

--- src/main/java/com/illiterate/illiterate/Controller/UserController.java
+++ src/main/java/com/illiterate/illiterate/Controller/UserController.java
@@ -6,6 +6,8 @@ import com.illiterate.illiterate.JWT.JWTUtil;
 import com.illiterate.illiterate.Service.CustomUserDetailsService;
 import com.illiterate.illiterate.Service.JoinService;
 import com.illiterate.illiterate.Service.LoginService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +16,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,15 +24,16 @@ import java.util.Map;
 @RequestMapping("/api")
 public class UserController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
     private final JoinService joinService;
-    private LoginService loginService;
+    private final AuthenticationManager authenticationManager;
+    private final JWTUtil jwtUtil;
 
-    public UserController(JoinService joinService) {
+    public UserController(JoinService joinService, AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
         this.joinService = joinService;
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
+
 
     @PostMapping("/join")
     public String registerUser(@RequestBody JoinDto joinDTO) {
@@ -38,8 +42,19 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public String LoginUser(@RequestParam String id, @RequestParam String password){
-        System.out.println("id : " + id + ", password : " + password);
-        return null;
+    public void loginUser(@RequestParam String id, @RequestParam String password, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // 로그인 요청을 LoginFilter를 통해 처리
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(id, password));
+
+            // 인증에 성공한 경우 JWT 토큰 생성
+            String token = response.getHeader("Authorization").substring(7); // "Bearer " 이후의 토큰 값만 추출
+
+            // 토큰을 클라이언트에게 반환
+            response.getWriter().write(token);
+        } catch (AuthenticationException e) {
+            // 인증에 실패한 경우
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
     }
 }
