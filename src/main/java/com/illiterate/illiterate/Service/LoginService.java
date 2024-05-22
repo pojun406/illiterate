265d3f5b9b4f16a61d13303a524edefa61a322b3
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-22 18:51:34 +0900

    .

--- src/main/java/com/illiterate/illiterate/Service/LoginService.java
+++ src/main/java/com/illiterate/illiterate/Service/LoginService.java
@@ -3,22 +3,36 @@ package com.illiterate.illiterate.Service;
 import com.illiterate.illiterate.Entity.User;
 import com.illiterate.illiterate.Repository.UserRepository;
 
-public class LoginService {
-    private final UserRepository userRepository;
+import com.illiterate.illiterate.Entity.User;
+import com.illiterate.illiterate.JWT.JWTUtil;
+import com.illiterate.illiterate.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
-    public LoginService(UserRepository userRepository) {
+@Service
+public class LoginService {
+
+    private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public LoginService(UserRepository userRepository, JWTUtil jwtUtil, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.jwtUtil = jwtUtil;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public String Login(String userid, String userpw){
-        User user = new User();
-        String id = user.getUserid();
-        String password = user.getPassword();
+    public String loginUser(String id, String password) {
+        User user = userRepository.findByUserid(id);
 
-        if(id.matches(userid) && password.matches(userpw)){
-            return "ok";
-        }else{
-            return "no";
+        if (user != null && bCryptPasswordEncoder.matches(password, user.getPassword())) {
+            // 인증에 성공한 경우 JWT 토큰 생성
+            return jwtUtil.createJwt(user.getUserid(), user.getRole(), 36000L); // 여기서 expirationTime은 토큰의 만료 시간입니다.
+        } else {
+            // 인증에 실패한 경우
+            return null;
         }
     }
 }
