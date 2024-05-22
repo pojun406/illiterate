3f0a300599a8558ce1c3782978be579691ddcdde
asowjdan <asowjdan@naver.com>
2024-05-22 14:50:31 +0900

    test

---
+++ src/test/java/com/illiterate/illiterate/IlliterateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illiterate.illiterate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlliterateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
