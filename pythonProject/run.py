c444294d6d3cdc7350366c61b783a64cfbfd7e0e
asowjdan <asowjdan@naver.com>
2024-05-27 22:35:17 +0900

    Main Function Implementation

--- pythonProject/run.py
+++
@@ -1,9 +0,0 @@
-from image_util import run1
-from letsgopaddle import run2
-from rungopaddle import main
-'''main이 rungopaddle(run3처럼)'''
-
-run1()
-run2()
-main()
-print("run complete")
--- pythonProject/rungopaddle.py
+++ pythonProject/rungopaddle.py
@@ -1,23 +1,24 @@
-from letsgopaddle import MyPaddleOCR
 import os
+import sys
+from letsgopaddle import MyPaddleOCR
 
-def main():
+def main(temp_file_path):
     ocr = MyPaddleOCR()
 
-    # 사용자로부터 이미지 파일 경로 입력 받기
-    base_directory = 'C:/Users/404ST011/PycharmProjects/pythonProject/photo'
-    image_filename = input("이미지 파일 이름을 입력하세요: ")
-    image_path = os.path.join(base_directory, image_filename)
-
-    if not os.path.exists(image_path):
-        print(f"파일을 찾을 수 없습니다: {image_path}")
+    if not os.path.exists(temp_file_path):
+        print(f"파일을 찾을 수 없습니다: {temp_file_path}")
         return
 
     # OCR 실행 및 디버그 모드 활성화
-    result = ocr.run_ocr(image_path, debug=True)
+    result = ocr.run_ocr(temp_file_path, debug=True)
     print("OCR 결과:")
     for item in result:
         print(item)
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python rungopaddle.py <TempFile>")
+        sys.exit(1)
+
+    temp_file_path = sys.argv[1]
+    main(temp_file_path)
