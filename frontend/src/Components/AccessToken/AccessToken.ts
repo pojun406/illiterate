472059c768290c4d71e587aaa8519ded83fad9d9
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-05-27 22:46:35 +0900

    AccessToken save logic

--- frontend/src/Components/AccessToken/AccessToken.ts
+++ frontend/src/Components/AccessToken/AccessToken.ts
@@ -1,15 +1,13 @@
-import axios from "axios";
-
-const AccessToken = () => {
+const AccessToken = (): string => {
     try {
         const token = localStorage.getItem('authToken');
         if (!token) {
             return '토큰이 없습니다.';
-        } else {
-            // 토큰이 있는 경우 Authorization 헤더 설정
-            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-            return '토큰이 있습니다.';
         }
+        // HTTP 요청 시 헤더에 Authorization 설정
+        const headers = new Headers();
+        headers.append("Authorization", `Bearer ${token}`);
+        return '토큰이 있습니다.';
     } catch (error: any) {
         return `에러가 발생했습니다: ${error.message}`;
     }
