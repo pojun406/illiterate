ea948a00bf7f2fe37cd0b7ee13d3ebaf7464e82c
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-28 16:12:06 +0900

    need redis DB
    after add redis

---
+++ src/main/java/com/illiterate/illiterate/member/Service/UserService.java
@@ -0,0 +1,85 @@
+package com.illiterate.illiterate.member.Service;
+
+import com.illiterate.illiterate.common.repository.RedisRepository;
+import com.illiterate.illiterate.member.DTO.request.JoinDto;
+import com.illiterate.illiterate.member.DTO.request.LoginDto;
+import com.illiterate.illiterate.member.DTO.response.LoginTokenDto;
+import com.illiterate.illiterate.member.Entity.User;
+import com.illiterate.illiterate.member.Repository.UserRepository;
+import com.illiterate.illiterate.member.exception.MemberException;
+import com.illiterate.illiterate.security.JWT.JWTProvider;
+import com.illiterate.illiterate.security.service.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.mapping.Join;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+import static com.illiterate.illiterate.common.enums.MemberErrorCode.DUPLICATED_MEMBER_EMAIL;
+import static com.illiterate.illiterate.common.enums.MemberErrorCode.DUPLICATED_MEMBER_PHONE_NUMBER;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTProvider jwtProvider;
+    private final AuthenticationManager authenticationManager;
+    private final RedisRepository redisRepository;
+
+    /**
+     * 회원 등록
+     */
+    @Transactional
+    public int joinUser(JoinDto joinDto) {
+        // 아이디 중복 체크
+        if (userRepository.existsByUserId(joinDto.getUserid())) {
+            throw new MemberException(DUPLICATED_MEMBER_EMAIL);
+        } else if (userRepository.existsByPhoneNumber(joinDto.getPhonenum())) {
+            throw new MemberException(DUPLICATED_MEMBER_PHONE_NUMBER);
+        }
+
+        User member = new User();
+        member.setUserid(joinDto.getUserid());
+        member.setPassword(passwordEncoder.encode(joinDto.getPassword()));
+        member.setPhonenum(joinDto.getPhonenum());
+        member.setUsername(joinDto.getUsername());
+
+        int id = userRepository.save(member).getId();
+
+        return id;
+    }
+
+    public LoginTokenDto login(LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                loginDto.getEmail(),
+                loginDto.getPassword()
+        );
+
+        Authentication authenticated = authenticationManager.authenticate(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetail = (UserDetailsImpl) authenticated.getPrincipal();
+
+        // accessToken, refreshToken 생성
+        String accessToken = jwtProvider.createAccessToken(userDetail);
+        String refreshToken = jwtProvider.createRefreshToken(userDetail);
+
+        LoginTokenDto loginTokenDto = new LoginTokenDto(accessToken, refreshToken);
+
+        // redis 토큰 정보 저장
+        redisRepository.saveToken(userDetail.getId(), refreshToken);
+
+        return loginTokenDto;
+    }
+}
---
+++ src/main/java/com/illiterate/illiterate/security/service/UserDetailsImpl.java
@@ -0,0 +1,72 @@
+package com.illiterate.illiterate.security.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.illiterate.illiterate.member.Entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private int id;
+    private String userid;
+    private String password;
+    private List<GrantedAuthority> authorities;
+
+    public static UserDetails from(User member) {
+        List<GrantedAuthority> authorities = member.getRoles() != null ?
+                List.of(new SimpleGrantedAuthority(member.getRoles().name())): null;
+
+        return new UserDetailsImpl(
+                member.getId(),
+                member.getUserid(),
+                member.getPassword(),
+                authorities
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
