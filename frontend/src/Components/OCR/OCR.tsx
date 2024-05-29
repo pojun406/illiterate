302d7e60ed4e827dc8a5912a54a9d7e20390d662
asowjdan <asowjdan@naver.com>
2024-05-29 16:21:49 +0900

    loading animation change

--- frontend/src/Components/OCR/OCR.tsx
+++ frontend/src/Components/OCR/OCR.tsx
@@ -16,10 +16,9 @@ const OCR: React.FC = () => {
         return (
             <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
                 <div className="flex flex-col justify-center items-center">
-                    <svg className="animate-spin h-24 w-24" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
+                    <div className="w-64 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: '100%', transition: 'width 3s' }}></div>
+                    </div>
                     <p className="text-white text-xl">로딩중...</p>
                 </div>
             </div>
@@ -30,10 +29,9 @@ const OCR: React.FC = () => {
         <div className="relative w-full h-full flex justify-center items-center">
             {loading ? (
                 <div className="flex flex-col justify-center items-center">
-                    <svg className="animate-spin h-24 w-24" viewBox="0 0 24 24">  // 크기를 h-24 w-24로 변경
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
+                    <div className="w-64 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: '100%', transition: 'width 3s' }}></div>
+                    </div>
                     <p className="text-white text-xl">로딩중...</p>
                 </div>
             ) : (
