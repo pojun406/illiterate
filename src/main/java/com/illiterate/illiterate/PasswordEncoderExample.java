9b0e3087466c67ad85b6c0243bb0cb0835385a82
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-05-29 15:48:57 +0900

    add mail and sql
    update sql ( add insert )
    password is 1234

--- src/main/java/com/illiterate/illiterate/PasswordEncoderExample.java
+++ src/main/java/com/illiterate/illiterate/PasswordEncoderExample.java
@@ -1,9 +1,9 @@
-package com.illiterate.illiterate.member.Service;
+package com.illiterate.illiterate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderExample {
     public static void main(String[] args) {
-        String rawPassword = "examplePasswordHash";
+        String rawPassword = "1234";
 
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String encodedPassword = passwordEncoder.encode(rawPassword);
