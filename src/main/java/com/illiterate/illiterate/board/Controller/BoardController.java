c94daf64b7e4f04b1bf469f9c753e76581496151
pojun406 <pojun406@naver.com>
2024-06-03 23:22:01 +0900

    Reapply "Merge branch 'Backend'"
    This reverts commit daac9c6956a782c8139fe73b1e4eb40f5b7da4fa.

---
+++ src/main/java/com/illiterate/illiterate/board/Controller/BoardController.java
@@ -0,0 +1,43 @@
+package com.illiterate.illiterate.board.Controller;
+
+
+
+import com.illiterate.illiterate.board.DTO.request.BoardRequestDto;
+import com.illiterate.illiterate.board.DTO.response.BoardResponseDto;
+import com.illiterate.illiterate.board.Entity.Board;
+import com.illiterate.illiterate.board.Repository.BoardRepository;
+import com.illiterate.illiterate.board.Service.BoardService;
+import com.illiterate.illiterate.common.response.BfResponse;
+import com.illiterate.illiterate.member.DTO.response.LoginTokenDto;
+import com.illiterate.illiterate.security.service.UserDetailsImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+    private final BoardRepository boardRepository;
+
+    @GetMapping("/api/board/posts")
+    public List<BoardResponseDto> getPosts() {
+        return boardService.getPosts();
+    }
+
+    @GetMapping("/api/board/posts/{id}")
+    public BoardResponseDto getPost(@PathVariable Long id) {
+        return boardService.getPost(id);
+    }
+
+    @PostMapping("/api/board/post")
+    public BoardResponseDto createPost(@RequestBody BoardRequestDto requestsDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return boardService.createPost(requestsDto, userDetails.getUser());
+    }
+
+}
