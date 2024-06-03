376ad7d0d0e30b51340b162411fb8f6481fd17c1
pojun406 <pojun406@naver.com>
2024-06-03 23:21:29 +0900

    Revert "Merge branch 'Backend'"
    This reverts commit 22dc59fa2f07882ca1bd4cb381579b3aca7953f3, reversing
    changes made to 2a933628e5e1e4191104a49e687af26057811af0.

--- src/main/java/com/illiterate/illiterate/member/Entity/User.java
+++ src/main/java/com/illiterate/illiterate/member/Entity/User.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
 
     private String userid;
     private String username;
