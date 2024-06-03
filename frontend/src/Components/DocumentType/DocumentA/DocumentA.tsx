ab9f7bcfaa921b070f9afb4d89a7f3c44f36076c
asowjdan <asowjdan@naver.com>
2024-06-03 16:59:44 +0900

    Login Implementation Completed

--- frontend/src/Components/DocumentType/DocumentA/DocumentA.tsx
+++ frontend/src/Components/DocumentType/DocumentA/DocumentA.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface DocumentAProps {
     data: any;
 }
 
 const DocumentA: React.FC<DocumentAProps> = ({ data }) => {
+    const [imageSrc, setImageSrc] = useState<string>('');
+
+    useEffect(() => {
+        const imageData = document.cookie.split('; ').find(row => row.startsWith('imageData='));
+        if (imageData) {
+            const base64Image = imageData.split('=')[1];
+            setImageSrc(`data:image/jpeg;base64,${decodeURIComponent(base64Image)}`);
+        }
+    }, []);
+
     if (!data) {
         return <div>데이터가 없습니다.</div>;
     }
@@ -12,7 +22,7 @@ const DocumentA: React.FC<DocumentAProps> = ({ data }) => {
     return (
         <div className="relative w-full h-full flex justify-center items-center">
             <div className="p-4">
-                <p className="text-lg">이미지: <img src={data.filePath} alt="이미지" /> </p>
+                <p className="text-lg">이미지: <img src={imageSrc || data.filePath} alt="이미지" /> </p>
                 <p className="text-lg">json: /mockup/ocrmockup.json </p>
                 <p className="text-lg">제목: {data[0].title} </p>
             </div>
