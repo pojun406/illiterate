4f82d0ae0cbac6c9489f1ae70e425be2b135fcc2
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-06-05 11:54:43 +0900

    findid, resetPassword

--- src/main/java/com/illiterate/illiterate/member/Entity/User.java
+++ src/main/java/com/illiterate/illiterate/member/Entity/User.java
@@ -22,6 +22,7 @@ public class User {
     private String username;
     private String password;
     private String email;
+    private String resetToken;
 
     @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(255)")
