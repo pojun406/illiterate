d3821eed9edaefa62f67d62f235853c69847d1d0
pojun406 <pojun406@naver.com>
2024-06-07 00:31:22 +0900

    error fix
    change sql query
    add Ocr repository
    fix ocr entity

--- src/main/java/com/illiterate/illiterate/ocr/Entity/OCR.java
+++ src/main/java/com/illiterate/illiterate/ocr/Entity/OCR.java
@@ -11,11 +11,14 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "board")
+@Table(name = "ocr_result")
 public class OCR {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
     /*@Column
