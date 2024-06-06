a51f3c7ab38dbdf16be514dcbd35e5f000fdcbf7
pojun406 <pojun406@naver.com>
2024-06-06 23:41:29 +0900

    OCR complete
    need test

--- src/main/java/com/illiterate/illiterate/ocr/DTO/response/OcrResponseDto.java
+++ src/main/java/com/illiterate/illiterate/ocr/DTO/response/OcrResponseDto.java
@@ -3,21 +3,12 @@ package com.illiterate.illiterate.ocr.DTO.response;
 import com.illiterate.illiterate.ocr.Entity.OCR;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class OcrResponseDto {
-    private byte[] before_img;
-    private byte[] after_img;
+    private String text;
+    private String imageUrl;
 
-    @Builder
-    private OcrResponseDto(OCR ocr){
-        this.before_img = ocr.getBimage();
-        this.after_img = ocr.getAimage();
-    }
-
-    /*public static OcrResponseDto from(OCR ocr) {
-        return OcrResponseDto.builder()
-                .board(board)
-                .build();
-    }*/
 }
