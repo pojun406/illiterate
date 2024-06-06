fbbed17dc95f66ab1c8b93df6aee6e379d24860c
pojun406 <pojun406@naver.com>
2024-06-06 17:21:08 +0900

    Update OcrController.java

--- src/main/java/com/illiterate/illiterate/ocr/Controller/OcrController.java
+++ src/main/java/com/illiterate/illiterate/ocr/Controller/OcrController.java
@@ -1,10 +1,14 @@
 package com.illiterate.illiterate.ocr.Controller;
 
+import com.illiterate.illiterate.common.response.BfResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RestController
 @RequiredArgsConstructor
 public class OcrController {
+    public ResponseEntity<BfResponse<?>> uploadWantImg(@RequestPart(value = "file", required = false) MultipartFile Image)
 }
