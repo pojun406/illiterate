59754e62230110d1645a25cb3c984a370f981bba
LBJ <pojun406@naver.com>
2024-06-10 20:59:33 +0900

    add anotataion
    to json request

--- src/main/java/com/illiterate/illiterate/member/Controller/UserController.java
+++ src/main/java/com/illiterate/illiterate/member/Controller/UserController.java
@@ -26,6 +26,12 @@ public class UserController {
 
     private final UserService userService;
 
+    /*
+    "userid": "testuser",
+    "username": "Test User",
+    "password": "password123",
+    "email": "testuser@example.com"
+     */
     //회원가입
     @PostMapping("/join")
     public ResponseEntity<BfResponse<?>> registerUser(@RequestBody JoinDto joinDTO) {
@@ -33,6 +39,10 @@ public class UserController {
                 .body(new BfResponse<>(CREATE,
                         Map.of("member_id", userService.joinUser(joinDTO))));
     }
+    /*
+         "userid": "testuser",
+        "password": "password123"
+     */
     //로그인
     @PostMapping("/login")
     public ResponseEntity<BfResponse<LoginTokenDto>> login(@Valid @RequestBody LoginDto loginDto) {
@@ -41,6 +51,9 @@ public class UserController {
         //return userService.login(loginDto);
     }
 
+    /*
+        "newPassword": "newpassword123"
+     */
     //리셋 패스워드(패스워드 리셋을 위한 페이지)
     @PatchMapping("/{userId}/password")
     public ResponseEntity<BfResponse<?>> resetPassword(
@@ -53,6 +66,9 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    /*
+        "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+     */
     // refresh토큰을 다시 설정해줌
     @PostMapping("/refresh")
     //public LoginTokenDto refreshAccessToken(
@@ -64,11 +80,18 @@ public class UserController {
     }
 
     // id 찾기 ( email을 입력하면 id를 찾을 수 있게 로직을 구성 )
+    /*
+    "userEmail"
+     */
     @PostMapping("/findId")
     public ResponseEntity<BfResponse<?>> findId(@RequestParam String userEmail){
         System.out.println(userService.findMemberId(userEmail));
         return ResponseEntity.ok(new BfResponse<>(userService.findMemberId(userEmail)));
     }
+
+    /*
+    "userId": "testuser"
+     */
 
     // 중복되는 id찾기
     @PostMapping("checkId")
@@ -76,6 +99,9 @@ public class UserController {
         return ResponseEntity.ok(new BfResponse<>(userService.checkId(userId)));
     }
 
+    /*
+    Request X
+     */
     // 회원정보 조회
     @GetMapping("/{userId}")
     public ResponseEntity<BfResponse<UserInfoDto>> getMemberInfo(
@@ -84,6 +110,11 @@ public class UserController {
         return ResponseEntity.ok(new BfResponse<>(userService.getUserInfo(memberId)));
     }
 
+
+    /*
+    "name": "Updated User",
+    "email": "updateduser@example.com"
+     */
     @PatchMapping("/{userId}")
     public ResponseEntity<BfResponse<?>> updateMemberInfo(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
@@ -94,6 +125,8 @@ public class UserController {
         return ResponseEntity.ok(new BfResponse<>(SUCCESS));
     }
 
+    //Request X
+    // 회원 삭제
     @DeleteMapping("/{userId}")
     public ResponseEntity<BfResponse<?>> inactivateMember(
             @AuthenticationPrincipal UserDetailsImpl userDetails,
