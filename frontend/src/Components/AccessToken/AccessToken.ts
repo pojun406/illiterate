1f28dbb6dd95d1f99fa7def37d0d96c0e88f53ac
LBJ <pojun406@naver.com>
2024-06-14 21:08:33 +0900

    JWT can read Refresh Token
    애매하게 오류있는거같아서 OCR 읽어내는쪽 마무리하고 방학중에 수정

--- frontend/src/Components/AccessToken/AccessToken.ts
+++ frontend/src/Components/AccessToken/AccessToken.ts
@@ -7,18 +7,10 @@ const fetchWithAuth = async (apiUrl: string, requestParameters: JSON | FormData)
     };
 
     const accessToken = await getToken('authToken');
-    if (!accessToken) {
-        return "인증 토큰이 없습니다.";
-    }
-
     const refreshToken = await getToken('refreshToken');
-    if (!refreshToken) {
-        return "인증 토큰이 없습니다.";
-    }
 
-    const id = await getToken('id');
-    if (!id) {
-        return "id값이 없습니다.";
+    if (!accessToken || !refreshToken) {
+        return "인증 토큰이 없습니다.";
     }
 
     const isFormData = requestParameters instanceof FormData;
@@ -29,25 +21,27 @@ const fetchWithAuth = async (apiUrl: string, requestParameters: JSON | FormData)
             'Authorization': `Bearer ${accessToken}`
         }
     };
-        const initialResponse = await axios.post(apiUrl, isFormData ? requestParameters : JSON.stringify(requestParameters), requestOptions);
 
-        if (initialResponse.status === 401) {
+    try {
+        const initialResponse = await axios.post(apiUrl, isFormData ? requestParameters : JSON.stringify(requestParameters), requestOptions);
+        return initialResponse;
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response && error.response.status === 401) {
             // 토큰이 만료된 경우, 갱신 시도
             try {
-                const refreshResponse = await axios.post('/refresh', {
-                    id: id
-                }, {
+                const refreshRequestOptions = {
                     headers: {
                         'Content-Type': 'application/json',
                         'Authorization': `Bearer ${refreshToken}`
                     }
-                });
+                };
 
-                const { accessToken: newAccessToken, refreshToken: newRefreshToken, id: newId } = refreshResponse.data.data;
-                if (newAccessToken && newRefreshToken && newId) {
+                const refreshResponse = await axios.post('/refresh', { refreshToken }, refreshRequestOptions);
+
+                const { accessToken: newAccessToken, refreshToken: newRefreshToken } = refreshResponse.data.data;
+                if (newAccessToken && newRefreshToken) {
                     localStorage.setItem('authToken', newAccessToken);
                     localStorage.setItem('refreshToken', newRefreshToken);
-                    localStorage.setItem('id', newId);
 
                     // 새로운 토큰으로 원래 요청 재시도
                     requestOptions.headers['Authorization'] = `Bearer ${newAccessToken}`;
@@ -59,9 +53,10 @@ const fetchWithAuth = async (apiUrl: string, requestParameters: JSON | FormData)
             } catch (refreshError) {
                 return "토큰 갱신 요청 중 오류가 발생했습니다.";
             }
+        } else {
+            return "요청 중 오류가 발생했습니다.";
         }
-
-        return initialResponse;
+    }
 };
 
 export default fetchWithAuth;
