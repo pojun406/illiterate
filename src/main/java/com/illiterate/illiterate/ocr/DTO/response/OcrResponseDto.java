083c66e687abb36315db530d4f7f6502b9a7297a
LBJ <pojun406@naver.com>
2024-06-23 00:34:41 +0900

    update readme & ocr post

--- src/main/java/com/illiterate/illiterate/ocr/DTO/response/OcrResponseDto.java
+++ src/main/java/com/illiterate/illiterate/ocr/DTO/response/OcrResponseDto.java
@@ -1,18 +1,32 @@
 package com.illiterate.illiterate.ocr.DTO.response;
 
 import com.illiterate.illiterate.ocr.Entity.OCR;
+import jdk.jshell.Snippet;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 public class OcrResponseDto {
     private Long id;
+    private Long userId;
     private String text;
-    private List<String> ocrResults;
+    private String ocrResults;
     private String imageUrl; // 이미지 경로
-    private String filteredText; // 필터링
+    private Date createdAt;
+
+    public static OcrResponseDto from(OCR ocr) {
+        return OcrResponseDto.builder()
+                .id(ocr.getId())
+                .userId(ocr.getUser().getId())
+                .imageUrl(ocr.getImagePath())
+                .ocrResults(ocr.getResult())
+                .createdAt(ocr.getCreatedAt())
+                .build();
+    }
 }
