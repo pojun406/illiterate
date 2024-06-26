d4ac3b25255ad9396465f68fcdc5ac629f61179d
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-06-27 05:18:44 +0900

    profile edit

--- frontend/src/Components/AccessToken/AccessToken.ts
+++ frontend/src/Components/AccessToken/AccessToken.ts
@@ -33,24 +33,8 @@ const fetchWithAuth = async (apiUrl: string, requestParameters?: any): Promise<A
                         'Authorization': `Bearer ${newAccessToken}`,
                         'Content-Type': 'application/json'
                     }
-
-                    // body : 'refreshToken' : '${refreshToken}'
-                };
-
-                const refreshResponse = await axios.post('/refresh', { refreshToken }, refreshRequestOptions);
-
-                const { accessToken: newAccessToken, refreshToken: newRefreshToken } = refreshResponse.data.data;
-                if (newAccessToken && newRefreshToken) {
-                    localStorage.setItem('authToken', newAccessToken);
-                    localStorage.setItem('refreshToken', newRefreshToken);
-
-                    // 새로운 토큰으로 원래 요청 재시도
-                    requestOptions.headers['Authorization'] = `Bearer ${newAccessToken}`;
-                    const retryResponse = await axios.post(apiUrl, isFormData ? requestParameters : JSON.stringify(requestParameters), requestOptions);
-                    return retryResponse;
-                } else {
-                    return "토큰 갱신에 실패했습니다.";
-                }
+                });
+                return retryResponse;
             } catch (refreshError) {
                 return "토큰 갱신 요청 중 오류가 발생했습니다.";
             }
@@ -61,4 +45,3 @@ const fetchWithAuth = async (apiUrl: string, requestParameters?: any): Promise<A
 };
 
 export default fetchWithAuth;
-
