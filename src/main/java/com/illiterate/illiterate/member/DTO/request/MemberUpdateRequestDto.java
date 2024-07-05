0b67715330484cdd4b2418c0e85ff9097f898193
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-07-06 06:30:22 +0900

    delete backend

--- src/main/java/com/illiterate/illiterate/member/DTO/request/MemberUpdateRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.illiterate.illiterate.member.DTO.request;
-
-public record MemberUpdateRequestDto(
-        String name,                            // 닉네임
-
-        String email
-) {
-}
