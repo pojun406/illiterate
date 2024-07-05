b7bda47f9d10e57113d13f59ee89a9eb75a193d4
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-07-06 06:38:32 +0900

    Revert "merge backend"
    This reverts commit b06431f2ed9f963115c1ed242f66ba114b970a93.

--- src/main/java/com/illiterate/illiterate/board/enums/StatusType.java
+++
@@ -1,6 +0,0 @@
-package com.illiterate.illiterate.board.enums;
-
-public enum StatusType {
-	WAIT, // 대기
-	COMPLETE // 완료
-}
--- src/main/java/com/illiterate/illiterate/security/Service/ReissueService.java
+++
@@ -1,9 +0,0 @@
-package com.illiterate.illiterate.security.Service;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.ResponseEntity;
-
-public interface ReissueService {
-    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response);
-}
