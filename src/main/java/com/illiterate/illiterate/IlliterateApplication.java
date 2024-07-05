c577f9571d9e2d60e5f036d03462b83580480a42
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-07-06 06:40:45 +0900

    reset backend

---
+++ src/main/java/com/illiterate/illiterate/IlliterateApplication.java
@@ -0,0 +1,14 @@
+package com.illiterate.illiterate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IlliterateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlliterateApplication.class, args);
+	}
+
+}
