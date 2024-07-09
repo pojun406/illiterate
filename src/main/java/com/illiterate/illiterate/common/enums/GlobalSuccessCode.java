6d49dc05f720f13ada5b433dbc7314f85deb28c6
LBJ <pojun406@naver.com>
2024-07-09 13:59:06 +0900

    fix checkid, securityconfig, refresh

--- src/main/java/com/illiterate/illiterate/common/enums/GlobalSuccessCode.java
+++ src/main/java/com/illiterate/illiterate/common/enums/GlobalSuccessCode.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum GlobalSuccessCode {
 	SUCCESS(200, "정상 처리되었습니다."),
-	CREATE(201, "정상적으로 생성되었습니다.");
+	CREATE(201, "정상적으로 생성되었습니다."),
+
+	BAD_REQUEST(400, "나가");
 
 	private final int code;
 	private final String message;
