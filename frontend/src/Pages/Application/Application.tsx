745362967351a8160c6d17ef6fa2eaf1bcc91e11
asowjdan <134794200+asowjdan@users.noreply.github.com>
2024-09-10 04:02:14 +0900

    Remove unnecessary files and configure results page

--- frontend/src/Pages/Application/Application.tsx
+++ frontend/src/Pages/Application/Application.tsx
@@ -1,16 +1,9 @@
-import React, { useState } from 'react';
 import ImageUpload from "../../Components/ImageUpload/ImageUpload";
-import Sidebar from "../../Components/Sidebar/Sidebar";
 
 const Application = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-
     return (
-        <div className="flex p-6 bg-gray-100" style={{ userSelect: 'none' }}>
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            <div className={`flex-auto bg-white rounded-lg shadow-md md:mx-4 transition-all ${sidebarOpen ? 'ml-64' : 'ml-0'} lg:ml-2`}>
-                <ImageUpload />
-            </div>
+        <div className="flex p-6 w-[1260px] justify-center items-center mx-auto">
+            <ImageUpload />
         </div>
     );
 };
