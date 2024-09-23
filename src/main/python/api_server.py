261118d9bfdbcd4beacb12ab986982f2fff21e78
mistysclouds <131018815+mistysclouds@users.noreply.github.com>
2024-09-23 16:38:51 +0900

    서버 이미지 전달 방식 수정
    이미지 파일->이미지 경로
    
    *이미지 경로로 받기로 한 걸 까먹고 있었음

--- src/main/python/api_server.py
+++ src/main/python/api_server.py
@@ -6,23 +6,17 @@ app = Flask(__name__)
 
 @app.route('/ocr', methods=['POST'])
 def ocr_api():
-    if 'file' not in request.files:
-        return jsonify({"error": "No file part"}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({"error": "No selected file"}), 400
-    if file:
-        filename = file.filename
-        file_path = os.path.join('temp', filename)
-        file.save(file_path)
-        try:
-            result = process_image(file_path)
-            return jsonify(result)
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
-        finally:
-            os.remove(file_path)
+    data = request.get_json()
+    if 'image_path' not in data:
+        return jsonify({"error": "No image path provided"}), 400
+    image_path = data['image_path']
+    if not os.path.exists(image_path):
+        return jsonify({"error": "Image path does not exist"}), 400
+    try:
+        result = process_image(image_path)
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    os.makedirs('temp', exist_ok=True)
     app.run(debug=True, host='0.0.0.0', port=5000)
