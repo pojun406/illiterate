347813f19c260b060d74ba045152032abfc07f2f
asowjdan <asowjdan@naver.com>
2024-10-14 14:04:51 +0900

    Update Mydocument.tsx

--- frontend/src/Pages/Mydocument/Mydocument.tsx
+++ frontend/src/Pages/Mydocument/Mydocument.tsx
@@ -1,7 +1,7 @@
 const Mydocument = () => {
     return (
         <div className="flex p-6 w-[1260px] justify-center items-center mx-auto">
-            <p>내 문의사항</p>
+            <p>내 문서함</p>
         </div>
     );
 };
